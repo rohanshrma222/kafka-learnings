703dda6cdaa13b01be2447ed5c31235cd81ec7eb
unknown <hackerviruse04@gmail.com>
2025-04-15 15:02:21 +0530

    added consumer

--- consumer.js
+++ consumer.js
@@ -1,8 +1,16 @@
 const { Kafka } = require('./client')
 
 async function init(){
-    const consumer = Kafka.consumer();
+    const consumer = Kafka.consumer({ groupId: "user-1 "});
     await consumer.connect();
 
-    await consumer.subscribe{}
+    await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition,message,heartbeat,pause}) =>{
+         console.log(`[${topic}]: PART:${partition}:`,message.value.toString())
+        }
+    })
 }
+
+init();
